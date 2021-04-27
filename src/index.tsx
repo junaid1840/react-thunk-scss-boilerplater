934e65f8cbcaa423e64f96f83245bd8b18b25607
Junaid Nazir <junaid_nazir15@yahoo.com>
2021-04-27 13:23:56 +0500

    TVM-400 - Configured ESLint and Prettier.
    Signed-off-by: junaidrt <junaid@rebeltechnology.io>

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/scss/global.scss';
-import Main from './layout/main/Main';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/scss/global.scss";
+import Main from "./layout/main/Main";
 
 ReactDOM.render(
   <React.StrictMode>
     <Main />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
