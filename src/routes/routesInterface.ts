ac4b63d1afc79f00029b0a98b6b6328f05f4f9b6
Junaid Nazir <junaid_nazir15@yahoo.com>
2021-04-28 14:42:10 +0500

    TVM-403 - Configured react router don. Added a basic Home route. Modified Main function name to Home.

---
+++ src/routes/routesInterface.ts
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { RouteComponentProps } from "react-router-dom";
+
+export interface iProps extends RouteComponentProps {
+  routes?: iRoutes[];
+}
+
+export default interface iRoutes {
+  // basic props
+  path: string;
+  exact: boolean;
+  component: FC<iProps>;
+  // Sub routes
+  routes?: iRoutes[];
+  // Redirect path
+  redirect?: string;
+  // If router is private, this is going to be true
+  private?: boolean;
+}
