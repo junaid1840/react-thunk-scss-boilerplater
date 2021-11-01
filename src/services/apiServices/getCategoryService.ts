3f3b466c1fe09307ab9209a50676b5c07916e0c0
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-01 18:44:08 +0500

    change return type for the service.

--- src/services/apiServices/getCategoryService.ts
+++ src/services/apiServices/getCategoryService.ts
@@ -1,6 +1,7 @@
 import apiManager from "../api-manager/apiManager";
+import { iApiResponse } from "../api-manager/apiManagerInterfaces";
 
-export const getCategoryService = async () => {
+export const getCategoryService = async (): Promise<iApiResponse> => {
   const url = `list/categories`;
   const { data } = await apiManager.request(url, {}, "GET");
   return data;
