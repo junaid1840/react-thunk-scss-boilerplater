0accf1cfcddf0067fb6e82730d8819184f5b2139
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-01 18:42:14 +0500

    revamp api utility for api manager.

--- src/services/api-manager/apiUtilities.ts
+++ src/services/api-manager/apiUtilities.ts
@@ -1,77 +1,90 @@
-import { iErrorHandler, iFailedRequest } from "./apiManagerInterfaces";
-// import axiosInstance from "./axiosInstance";
-// let isRefreshing = false
-// let failedApiQueue = []
-//
-//
-// const refreshUserToken = async (failedRequest: iFailedRequest) => {
-//     // if refresh token request has already been sent
-//     if (isRefreshing) {
-//         return new Promise( (resolve, reject) => {
-//             failedApiQueue.push ({resolve,  reject})
-//         } ).then( token => {
-//             failedRequest.response.config.headers['authorization-token'] = `${token}`
-//             return axiosInstance(failedRequest.config);
-//         }).catch( () => {
-//             throwError(failedRequest)
-//         })
-//     }
-//     console.log(failedRequest)
-//     isRefreshing = true
-//     Vue.$toaster.info('Renewing your Login Session. Please wait ...')
-//     const tokenRefreshResponse = await requestToRefreshUserTokens()
-//     const userTokens = tokenRefreshResponse.data.data
-//     // assign the new token to the failed api calls header so that we can resend it
-//     failedRequest.response.config.headers['authorization-token'] = `${userTokens.token}`
-//     store.commit('UPDATE_USER_TOKEN', userTokens)
-//     updateTokenInLocalStorageAndAxiosHeader(userTokens)
-//     // empty the Failed api queue
-//     remakeFailedApiCalls(userTokens.token)
-//     isRefreshing = false
-//     // returning the failed api call config in axios to re make the request
-//     return axiosInstance(failedRequest.config)
-// }
-//
-// const remakeFailedApiCalls = (token = null) => {
-//     failedApiQueue.forEach( failedAPiPromise => {
-//         if (token) {
-//             failedAPiPromise.resolve(token);
-//         } else {
-//             failedAPiPromise.reject();
-//         }
-//     });
-//     failedApiQueue = [];
-// }
-//
-// export const updateTokenInLocalStorageAndAxiosHeader = (data) => {
-//     localStorage.setItem("token", data.token)
-//     localStorage.setItem("refresh_token", data.refresh_token)
-//     // TODO remove this when delete old API manager
-//     axiosInstance.defaults.headers['authorization-token'] = data.token
-//     // newAxiosInstance.defaults.headers['authorization-token'] = data.token
-// }
-//
-// const closeLoaderAndLogoutUser = async (failedRequest: iFailedRequest) => {
-//     if (localStorage['refresh_token']) {
-//         // Vue.$toaster.warning('Seems like your session has been ended')
-//         // store.commit("Error_TOASTER_CHECK")
-//     }
-//     // await router.push('/logout')
-//     throwError(failedRequest)
-// }
-const throwError = (failedRequest: iFailedRequest): any => {
-  // throw new Error(failedRequest.response.data.message);
-  console.log(failedRequest);
+import { AxiosResponse } from "axios";
+import { iApiResponse, iErrorHandler } from "./apiManagerInterfaces";
+import apiManager from "./apiManager";
+import { axiosInstance } from "./axiosInstance";
+
+let isRefreshing = false;
+let failedApiQueue: {
+  resolve: (value: unknown) => void;
+  reject: () => void;
+}[] = [];
+
+const refreshTokenAction = (): iApiResponse => {
+  console.log("Perform Refresh Token Action");
+  return {} as iApiResponse;
 };
+
+const throwError = (
+  failedRequest: AxiosResponse<iApiResponse>
+): iApiResponse => {
+  return failedRequest.data;
+};
+
+const addToQueue = (failedRequest: AxiosResponse<iApiResponse>) =>
+  new Promise((resolve, reject) => {
+    failedApiQueue.push({ resolve, reject });
+  })
+    .then((token) => {
+      failedRequest.config.headers = {
+        ...failedRequest.config.headers,
+        "access-token": `${token}`,
+      };
+      return axiosInstance(failedRequest.config);
+    })
+    .catch(() => {
+      throwError(failedRequest);
+    });
+
+const remakeFailedApiCalls = (token = null) => {
+  failedApiQueue.forEach((failedAPiPromise) =>
+    token ? failedAPiPromise.resolve(token) : failedAPiPromise.reject()
+  );
+  failedApiQueue = [];
+};
+
+const requestToRefreshUserTokensService = (): Promise<
+  AxiosResponse<iApiResponse>
+> => {
+  const url = `user/refreshToken`;
+  const refreshToken = localStorage.getItem("refreshToken");
+  return apiManager.request(url, { refreshToken }, "POST");
+};
+
+const refreshUserToken = async (
+  failedRequest: AxiosResponse<iApiResponse>
+): Promise<iApiResponse | AxiosResponse<never> | void> => {
+  // if refresh token request has already been sent
+  if (isRefreshing) {
+    return failedRequest.config.url === "user/refreshToken"
+      ? refreshTokenAction()
+      : addToQueue(failedRequest);
+  }
+  isRefreshing = true;
+  const { data } = await requestToRefreshUserTokensService();
+  // assign the new token to the failed api calls header so that we can resend it
+  failedRequest.config.headers = {
+    ...failedRequest.config.headers,
+    "access-token": `${data.response}`,
+  };
+  // setAccessTokenInLocalStorage(userTokens);
+  // empty the Failed api queue
+  remakeFailedApiCalls(data.response);
+  isRefreshing = false;
+  // returning the failed api call config in axios to re make the request
+  return axiosInstance(failedRequest.config);
+};
+
+export const customDispatch = (
+  dispatchAction: any,
+  response: iApiResponse
+): iApiResponse => {
+  !response.error && dispatchAction(response.response);
+  return response;
+};
+
 export const ApiErrorHandler: iErrorHandler = {
   // when Jwt Expires and api returns locked error
-  423: throwError,
-  // when user is unauthorized
-  401: throwError,
-  // Forbidden when user provides wrong data
-  403: throwError,
-  // Not Found
-  404: throwError,
-  // Not Acceptable
-  406: throwError,
+  999: throwError,
+  // refresh token logic
+  412: refreshUserToken,
 };
