415a7ebf6cc2db481540d67ae990e41977866ac0
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-01 18:47:25 +0500

    wrap route with sub routes with global provider.

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./main.scss";
 import routes from "../../routes/routes";
 import RouteWithSubRoutes from "../../routes/routeWithSubRoutes";
+import { GlobalProvider } from "../../services/context/GlobalProvider";
 
 export const Main = (): JSX.Element => {
   return (
-    <>{<RouteWithSubRoutes routes={routes} />}</>
+    <>
+      <GlobalProvider>{<RouteWithSubRoutes routes={routes} />}</GlobalProvider>
+    </>
+
     // Add footer here
   );
 };
