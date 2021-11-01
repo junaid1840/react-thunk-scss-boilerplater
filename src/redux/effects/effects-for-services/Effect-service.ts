5b1f9d94f369d959ba2fc23dc8e1d837baa2a9cf
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-01 18:43:48 +0500

    use custom dispatch to dispatch action in Effect.

--- src/redux/effects/effects-for-services/Effect-service.ts
+++ src/redux/effects/effects-for-services/Effect-service.ts
@@ -2,10 +2,17 @@ import { Dispatch } from "redux";
 import { getCategoryService } from "../../../services/apiServices/getCategoryService";
 import { getCategoryDropdownAction } from "../../actions/book/bookAction";
 import { iGetCategoryOptionsActionType } from "../../types/book/bookType";
+import { customDispatch } from "../../../services/api-manager/apiUtilities";
+import { iApiResponse } from "../../../services/api-manager/apiManagerInterfaces";
 
 export const getCategoryDropDownEffect =
   () =>
-  async (dispatch: Dispatch<iGetCategoryOptionsActionType>): Promise<void> => {
-    const data = getCategoryService();
-    dispatch(getCategoryDropdownAction(data));
+  async (
+    dispatch: Dispatch<iGetCategoryOptionsActionType>
+  ): Promise<iApiResponse> => {
+    const response = await getCategoryService();
+    return customDispatch(
+      dispatch(getCategoryDropdownAction(response.response)),
+      response
+    );
   };
