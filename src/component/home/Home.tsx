4d29e7d3556ff34464437afeab48a3265762aebc
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-01 18:46:50 +0500

    add dummy data in home for testing.

--- src/component/home/Home.tsx
+++ src/component/home/Home.tsx
@@ -1,15 +1,20 @@
-import React, { FC, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { FC, useContext } from "react";
 import { getCategoryDropDownEffect } from "../../redux/effects/effects-for-services/Effect-service";
+import { GlobalContext } from "../../services/context/GlobalContext";
 
 export const Home: FC = () => {
-  const dispatch = useDispatch();
-  const getCategory = () => dispatch(getCategoryDropDownEffect());
+  const { showLoader, setShowLoader, globalDispatch } =
+    useContext(GlobalContext);
+  const getCategory = async () => {
+    setShowLoader(true);
+    await globalDispatch(getCategoryDropDownEffect());
+  };
 
   return (
     <>
       <h1>Home</h1>
-      <button onClick={getCategory}>Go Error</button>
+      {showLoader && <h1>Hello</h1>}
+      <button onClick={() => getCategory()}>Go Error</button>
     </>
   );
 };
