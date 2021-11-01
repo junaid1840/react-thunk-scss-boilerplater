86019a2bd70f60c1f22690bcb271c22bf176e891
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-01 18:29:59 +0500

    change return type for the error handler and add new interface for the api response.

--- src/services/api-manager/apiManagerInterfaces.ts
+++ src/services/api-manager/apiManagerInterfaces.ts
@@ -1,7 +1,14 @@
 import { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export interface iErrorHandler {
-  [statusCode: number]: (failedRequest: iFailedRequest) => {} | never;
+  [statusCode: number]: (failedRequest: AxiosResponse<iApiResponse>) => void;
+}
+
+export interface iApiResponse {
+  response: iApiResponse;
+  error: boolean;
+  message: string;
+  action: string;
 }
 
 export interface iFailedRequest {
