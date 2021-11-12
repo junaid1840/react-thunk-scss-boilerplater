15c9992328c4d282945c99e502da08bb44e1d17b
Muhammad-Muneeb-Zafar <muneeb.zafar@rebeltechnology.io>
2021-11-12 18:13:45 +0500

    change type of effect in context.

--- src/services/context/GlobalContext.tsx
+++ src/services/context/GlobalContext.tsx
@@ -1,16 +1,14 @@
 import React, { createContext } from "react";
 import { iApiResponse } from "../api-manager/apiManagerInterfaces";
-import { iGetCategoryOptionsActionType } from "../../redux/types/book/bookType";
+import { bookActionTypes } from "../../redux/types/book/bookType";
 import { Dispatch } from "redux";
 
 export interface iGlobalContext {
   showLoader: boolean;
   setShowLoader: React.Dispatch<boolean>;
   globalDispatch: (
-    effect: (
-      dispatch: Dispatch<iGetCategoryOptionsActionType>
-    ) => Promise<iApiResponse>
-  ) => Promise<void>;
+    effect: (dispatch: Dispatch<bookActionTypes>) => Promise<iApiResponse>
+  ) => Promise<iApiResponse>;
 }
 
 export const GlobalContext = createContext({} as iGlobalContext);
