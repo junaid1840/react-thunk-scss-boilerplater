808018dc6c1f2349e9f939ddd85c5c77c6139fd0
Arslan <arslan@rebeltechnology.io>
2021-11-22 17:24:42 +0500

    J0-30 - merge some change from previous PR

--- src/component/accordion/AccordionItems.tsx
+++ src/component/accordion/AccordionItems.tsx
@@ -1,13 +1,25 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 type Props = {
     answer: string,
     question: string,
 };
 export const AccordionItems: FC<Props> = ({ question, answer }) => {
     const [isActive, setIsActive] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+    const handleClickOutside = (event: { target: any; }) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setIsActive(false);
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, true);
+        return () => {
+            document.removeEventListener("click", handleClickOutside, true);
+        };
+    });
     return (
-        <div className="accordions-box" onClick={() => setIsActive(!isActive)}>
-            <div className="accordions-title"><h2>{question}</h2> <span>{isActive ? '-' : '+'}</span></div>
+        <div className="accordions-box" ref={ref}>
+            <div className="accordions-title" onClick={() => setIsActive(!isActive)}><h2>{question}</h2> <span>{isActive ? '-' : '+'}</span></div>
             <div className={`accordions-content-wrap ${isActive ? 'active': ''}`}>
                 <div className="accordions-content"><p>{answer}</p></div>
             </div>
