a14d5a85c31fcd910865f218e3e7bab0685bf5bc
Arslan <arslan@rebeltechnology.io>
2021-11-22 14:51:42 +0500

    J0-29 - code and scss related issues has been resolved.

--- src/component/Header/Logo.tsx
+++ src/component/Header/Logo.tsx
@@ -3,8 +3,6 @@ import {NavLink} from "react-router-dom";
 export const Logo: FC = () => {
     let logo = "/images/logo.png";
     return (
-        <div className="header-logo">
-            <NavLink to="/"><img src={logo} alt="logo" /></NavLink>
-        </div>
+        <NavLink className="header-logo" to="/"><img src={logo} alt="logo" /></NavLink>
     );
 }
