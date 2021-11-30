55c0bfaa7db0c8db8b283613aae89ad572501219
Arslan <arslan@rebeltechnology.io>
2021-11-30 14:09:58 +0500

    J0-30 - minor updates related to accordion

--- src/component/accordion/AccordionItems.tsx
+++ src/component/accordion/AccordionItems.tsx
@@ -1,28 +1,32 @@
-import React, {FC, useEffect, useRef, useState} from 'react';
+import React, { FC, useEffect, useRef, useState } from "react";
 type Props = {
-    answer: string,
-    question: string,
+  answer: string;
+  question: string;
 };
 export const AccordionItems: FC<Props> = ({ question, answer }) => {
-    const [isActive, setIsActive] = useState(false);
-    const ref = useRef<HTMLDivElement>(null);
-    const handleClickOutside = (event: { target: any; }) => {
-        if (ref.current && !ref.current.contains(event.target)) {
-            setIsActive(false);
-        }
+  const [isActive, setIsActive] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const handleClickOutside = (event: { target: any }) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsActive(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
     };
-    useEffect(() => {
-        document.addEventListener("click", handleClickOutside, true);
-        return () => {
-            document.removeEventListener("click", handleClickOutside, true);
-        };
-    });
-    return (
-        <div className="accordions-box" ref={ref}>
-            <div className="accordions-title" onClick={() => setIsActive(!isActive)}><h2>{question}</h2> <span>{isActive ? '-' : '+'}</span></div>
-            <div className={`accordions-content-wrap ${isActive ? 'active': ''}`}>
-                <div className="accordions-content"><p>{answer}</p></div>
-            </div>
+  });
+  return (
+    <div className="accordions-box" ref={ref}>
+      <div className="accordions-title" onClick={() => setIsActive(!isActive)}>
+        <h2>{question}</h2> <span>{isActive ? "-" : "+"}</span>
+      </div>
+      <div className={`accordions-content-wrap ${isActive ? "active" : ""}`}>
+        <div className="accordions-content">
+          <p>{answer}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
